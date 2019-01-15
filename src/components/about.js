d711bbea6c8b1dd6eb7ee4cdb22724c37c44e278
MichaelCapo23 <123pie23@gmail.com>
2019-01-14 16:59:55 -0800

    all pages are routed and functional

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">About Movie Quotes</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, alias debitis delectus dolorem ducimus eos excepturi id nam quasi, quia sed sequi ut veniam! Accusantium atque doloribus dolorum eaque repellendus!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore in ipsum officiis ratione repellendus sit tempore. Corporis itaque natus perferendis!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et id maxime possimus quisquam? Ipsum.</p>
+    </div>
+)
--- src/components/movie_list.js
+++ src/components/movie_list.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 class movieList extends Component {
+    isPrivate = this.props.match.params.type === 'private';
+
+
     render() {
+        console.log("Is Private", this.isPrivate);
         return(
             <div>
-                <h1 className="center">Movie List</h1>
+                <h1 className="center">
+                <div className={"grey-text"}>{this.isPrivate && <small>Private</small>}</div>
+                Movie List
+                </h1>
             </div>
         )
     }
